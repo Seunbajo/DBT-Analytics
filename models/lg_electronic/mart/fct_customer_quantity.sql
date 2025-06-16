a87403ca9bd54858e0ba89c7415b6e9c4b8a8caa
Seun Olubajo <seunbajoshola@gmail.com>
2025-06-16 20:57:15 +0000

    Build model fct_quantity_customers

---
+++ models/lg_electronic/mart/fct_customer_quantity.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized = 'table'
+        , tags = ['exchange_rate']
+    )
+}}
+
+with sales as (
+    select 
+        customer_id
+        , currency_code
+        , count(quantity) as quantity
+    from {{ ref('stg_electronic_sales') }}
+    group by customer_id, currency_code
+)
+
+, customers as (
+    select * from {{ ref('dim_electronic_customers') }}
+)
+
+, final as (
+    select 
+        sales.customer_id
+        , sales.currency_code
+        , sales.quantity
+        , customers.customer_name
+        , customers.city
+        , customers.state
+        , customers.country
+    from sales
+    left join customers
+        on sales.customer_id = customers.customer_id
+)
+
+select * from final
