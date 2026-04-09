b60ab27163a491d5af4ebaec518b22a1f2a85648
Seun Olubajo <seunbajoshola@gmail.com>
2026-04-09 16:10:12 +0000

    Working on ec staging

---
+++ models/Ecomm_marketing/staging/ec/stg_ec_orders.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized = 'table'
+        , tags = ['orders']
+    )
+}}
+
+with staging as (
+    select * from {{ source('dbt_projects_services', 'ec_orders') }}
+),
+
+final as (
+    select * from staging
+)
+
+select * from final
